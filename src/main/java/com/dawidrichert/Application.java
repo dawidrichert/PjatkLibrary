33a2433d434159ea734a4847edfca572ceb09b9a
Dawid Richert <dawidrichert@gmail.com>
2015-05-14 23:28:32 +0200

    Added the best lines ever!

--- src/main/java/com/dawidrichert/Application.java
+++ src/main/java/com/dawidrichert/Application.java
@@ -8,12 +8,14 @@ import java.awt.*;
 public class Application {
 
     public static void main(String[] args) throws InstantiationException {
+
         try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         }
         catch (UnsupportedLookAndFeelException | ClassNotFoundException | IllegalAccessException e) {
             System.out.println("Failed to set System Look and Feel Theme.");
         }
+
         EventQueue.invokeLater(() -> {
             MainForm mainForm = new MainForm();
             mainForm.setVisible(true);
